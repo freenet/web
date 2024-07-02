2fc3e843c72fcb03f084ab60b9d17bd514f448a2
Ian Clarke <github@ian.33mail.com>
2024-07-02 15:54:10 -0500

    Implemented Content-Type-Options header using Rocket's AdHoc fairing.

--- rocket-api/src/main.rs
+++ rocket-api/src/main.rs
@@ -5,7 +5,8 @@ mod routes;
 mod stripe_handler;
 
 use rocket::fairing::AdHoc;
-use rocket::shield::{Shield, XssFilter, Referrer, ContentTypeOptions};
+use rocket::shield::{Shield, XssFilter, Referrer};
+use rocket::fairing::AdHoc;
 
 #[launch]
 fn rocket() -> _ {
@@ -19,7 +20,9 @@ fn rocket() -> _ {
         .attach(Shield::new()
             .enable(XssFilter::EnableBlock)
             .enable(Referrer::NoReferrer)
-            .enable(Referrer::StrictOriginWhenCrossOrigin)
-            .enable(ContentTypeOptions::NoSniff))
+            .enable(Referrer::StrictOriginWhenCrossOrigin))
+        .attach(AdHoc::on_response("Content-Type-Options Header", |_, res| Box::pin(async move {
+            res.set_raw_header("X-Content-Type-Options", "nosniff");
+        })))
         .mount("/", routes::routes())
 }
