22f981a333e8b7457874f2b3dc5acdd9cdfad83c
Ian Clarke <github@ian.33mail.com>
2024-07-07 10:19:16 -0400

    Refactored Stripe payment intent update logic to use a more concise API.

--- rocket-api/src/stripe_handler.rs
+++ rocket-api/src/stripe_handler.rs
@@ -38,10 +38,8 @@ pub async fn sign_certificate(request: SignCertificateRequest) -> Result<SignCer
     // Mark the payment intent as used for certificate signing
     let mut metadata = HashMap::new();
     metadata.insert("certificate_signed".to_string(), "true".to_string());
-    let params = stripe::PaymentIntentUpdateParams {
-        metadata: Some(Metadata::from(metadata)),
-        ..Default::default()
-    };
+    let params = stripe::UpdatePaymentIntent::new()
+        .metadata(Some(Metadata::from(metadata)));
     pi = PaymentIntent::update(&client, &pi.id, params).await?;
 
     // Load the server's signing key
