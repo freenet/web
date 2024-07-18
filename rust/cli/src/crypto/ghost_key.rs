419e1011473b86e565ef459aa8398ddb45086077
Ian Clarke <ian.clarke@gmail.com>
2024-07-18 00:10:55 -0500

    Implemented GhostkeySigningData struct to hold data required for ghostkey signing.

--- rust/cli/src/crypto/ghost_key.rs
+++ rust/cli/src/crypto/ghost_key.rs
@@ -40,6 +40,13 @@ pub struct GhostkeyCertificate {
     pub signature: Vec<u8>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GhostkeySigningData {
+    pub version: u8,
+    pub delegate_certificate: Vec<u8>,
+    pub ghostkey_verifying_key: Vec<u8>,
+}
+
 pub fn generate_ghostkey(delegate_certificate: &str, delegate_signing_key: &str) -> Result<String, CryptoError> {
     info!("Generating ghostkey");
     
