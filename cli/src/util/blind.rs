dc8127ca43dce3c6615e2e6427964f7884b60926
Ian Clarke <ian.clarke@gmail.com>
2024-07-28 15:36:20 -0500

    Removed duplicate function definition in the blind module and added a temporary directory import in the armorable module.

--- cli/src/util/blind.rs
+++ cli/src/util/blind.rs
@@ -1,3 +1,6 @@
 pub fn example_function() {
     println!("This is an example function in the blind module.");
 }
+pub fn example_function() {
+    println!("This is an example function in the blind module.");
+}
