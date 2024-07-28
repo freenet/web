ba0b136fa5b8d43216add00c66c738e30c353d9e
Ian Clarke <ian.clarke@gmail.com>
2024-07-28 15:36:58 -0500

    Added a duplicate example function in the blind module and imported env_logger in the util module.

--- cli/src/util/blind.rs
+++ cli/src/util/blind.rs
@@ -7,3 +7,6 @@ pub fn example_function() {
 pub fn example_function() {
     println!("This is an example function in the blind module.");
 }
+pub fn example_function() {
+    println!("This is an example function in the blind module.");
+}
