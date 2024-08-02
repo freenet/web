ede33a0f7f1c292996ceeab86fd914f5ebecd534
Ian Clarke <ian.clarke@gmail.com>
2024-08-02 17:02:55 -0500

    fix: Use `Server` instead of `axum::Server` in `main.rs`

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -131,7 +131,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     info!("Listening on {}", addr);
-    axum::Server::bind(&addr)
+    Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
