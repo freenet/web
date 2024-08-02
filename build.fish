a092c2fb21b2b1e5fc7b8e320272aa763f1c6cc4
Ian Clarke <ian.clarke@gmail.com>
2024-08-02 17:15:41 -0500

    migrate to axum, integration test passes

--- build.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-
-# Note: should use --release in production
-cd rust/gkwasm
-wasm-pack build --target web --out-dir ../../hugo-site/static/wasm
-cd ../..
--- rust/gklib/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod armorable;
-pub mod delegate_certificate;
-pub mod ghost_key_certificate;
-pub mod errors;
-pub mod util;
