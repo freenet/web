6e12a208349b04bacac3f668ec2fee921e36b111
Ian Clarke <ian.clarke@gmail.com>
2024-08-06 08:15:01 -0500

    commit wasm output to see if this solves the problem - but could cause other issues

---
+++ hugo-site/static/wasm/gkwasm_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function wasm_generate_keypair_and_blind(a: number, b: number, c: number, d: number): number;
+export function wasm_generate_ghost_key_certificate(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
