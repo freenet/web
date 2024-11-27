e33cc15114b7422999473d9fe6043de71b67b131
Ian Clarke <ian.clarke@gmail.com>
2024-11-26 20:34:01 -0600

    refactor: Reduce route generation interval from 1500ms to 500ms

--- hugo-site/static/js/small-world-routing.js
+++ hugo-site/static/js/small-world-routing.js
@@ -290,7 +290,7 @@ async function initVisualization() {
                         if (isPlaying) {
                             startNewRoute();
                         }
-                    }, 1500);
+                    }, 500);
                     return;
                 }
                 
